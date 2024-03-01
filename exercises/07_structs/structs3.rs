38a572e7d39ee04e04ca7bca97a7a8cf03c43e5b
Aviran Abady <AviranAbady@users.noreply.github.com>
2024-03-01 22:21:48 +0000

    Pending changes exported from your codespace

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint structs3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -31,12 +30,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: u32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -68,7 +67,7 @@ mod tests {
         let sender_country = String::from("Canada");
         let recipient_country = sender_country.clone();
 
-        let package = Package::new(sender_country, recipient_country, 1200);
+        let package = Package::new(sender_country, recipient_country, 1200);       
 
         assert!(!package.is_international());
     }
--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,10 +16,28 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+
+use std::fmt;
+use Grade::Numerical;
+use Grade::Literal;
+
+enum Grade {
+    Literal(String),
+    Numerical(f32)
+}
+
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Literal(ref l) =>  write!(f, "{}", l),
+            Numerical(n) => write!(f,"{}", n),
+        }
+    }    
+}
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -38,7 +56,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Numerical(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +70,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Literal("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
